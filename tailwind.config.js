8f7444403f2b2f306f179198e97ea5713dfe0010
Success Divine Onyechinemere <divinesuccess228@gmail.com>
2022-09-09 02:03:50 +0100

    update config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens:{
+
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+
+    },
+    
+    extend: {
+      colors:{
+        veryDarkBlue: 'hsl(243, 87%, 12%)',
+        desaturatedBlue: 'hsl(238, 22%, 44%)',
+        moderateBlue: 'hsl(224, 93%, 58%)',
+        moderateCyan: 'hsl(170, 45%, 43%)',
+        lightGrayishBlue: 'hsl(240, 75%, 98%)',
+        lightGray: 'hsl(0, 0%, 75%)',
+      }
+    },
   },
   plugins: [],
 }
