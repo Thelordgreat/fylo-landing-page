ee9a426c1073b3c7e5b6c08b44ae614a2365b1d1
Thelordgreat <greatokafor15@gmail.com>
2022-09-09 14:58:17 +0100

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,6 @@ module.exports = {
         moderateCyan: 'hsl(170, 45%, 43%)',
         lightGrayishBlue: 'hsl(240, 75%, 98%)',
         lightGray: 'hsl(0, 0%, 75%)',
-        boxshadow: 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
       }
     },
   },
