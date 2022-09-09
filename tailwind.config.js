f1f7740a92e88e6329a3074c89d59e6d54da0aff
GodwinAsuquo <godwinedetauquo@gmail.com>
2022-09-09 17:33:57 +0100

    create cta and footer section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    fontSize: {
+      xs: ['10px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
+  
+
     screens:{
 
       sm: '480px',
