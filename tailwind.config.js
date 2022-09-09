83e56fb4dc497964f1163b4e96c862d3e818e519
GodwinAsuquo <godwinedetauquo@gmail.com>
2022-09-09 18:31:59 +0100

    adjust the positioning of the footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    fontSize: {
-      xs: ['10px', '16px'],
-      sm: ['14px', '20px'],
-      base: ['16px', '24px'],
-      lg: ['20px', '28px'],
-      xl: ['24px', '32px'],
-    },
   
 
     screens:{
